5e6ef359cb119e1829b180aed5b6b6361dededf7
rajatAgrawalHub <rajat.agrawal2022@vitstudent.ac.in>
2024-06-17 11:45:18 +0530

    first commit

---
+++ First_Draft/Icetite'24 website  code/bolt/js/custom.js
@@ -0,0 +1,27 @@
+(function ($) {
+
+  "use strict";
+
+    // PRE LOADER
+    $(window).load(function(){
+      $('.preloader').fadeOut(1000); // set duration in brackets    
+    });
+
+
+    // MENU
+    $('.menu-burger').on('click', function() {
+      $('.menu-bg, .menu-items, .menu-burger').toggleClass('fs');
+      $('.menu-burger').text() == "☰" ? $('.menu-burger').text('✕') : $('.menu-burger').text('☰');
+    });
+
+
+    // ABOUT SLIDER
+    $('#home').vegas({
+        slides: [
+            { src: 'images/code.jpg' }
+        ],
+        timer: false,
+        transition: [ 'zoomOut', ]
+    });
+
+})(jQuery);
---
+++ First_Draft/Icetite'24 website  code/js/main.js
@@ -0,0 +1,79 @@
+$(function() {
+  "use strict";
+
+  var nav_offset_top = $('header').height() + 50; 
+    /*-------------------------------------------------------------------------------
+	  Navbar 
+	-------------------------------------------------------------------------------*/
+
+	//* Navbar Fixed  
+    function navbarFixed(){
+        if ( $('.header_area').length ){ 
+            $(window).scroll(function() {
+                var scroll = $(window).scrollTop();   
+                if (scroll >= nav_offset_top ) {
+                    $(".header_area").addClass("navbar_fixed");
+                } else {
+                    $(".header_area").removeClass("navbar_fixed");
+                }
+            });
+        };
+    };
+    navbarFixed();
+
+
+  //------- mailchimp --------//  
+	
+
+
+  $('.img-gal').magnificPopup({
+    type: 'image',
+    gallery: {
+        enabled: true
+    }
+  });
+
+
+
+  if ($('.blogCarousel').length) {
+    $('.blogCarousel').owlCarousel({
+        loop: false,
+        margin: 30,
+        items: 1,
+        nav: true,
+        autoplay: 2500,
+        smartSpeed: 1500,
+        dots: false,
+        responsiveClass: true,
+        navText : ["<div class='left-arrow'><i class='ti-angle-left'></i></div>","<div class='right-arrow'><i class='ti-angle-right'></i></div>"],
+        responsive:{
+          0:{
+              items:1
+          },
+          1000:{
+              items:2
+          }
+      }
+    })
+  }
+  
+  
+});
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
